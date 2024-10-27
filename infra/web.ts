972edbf9d2bce0fd63899c7a431c625c03a41dca
Cesar Garcia <43119240+cesaralej@users.noreply.github.com>
2024-10-27 08:44:22 +0100

    domain setup

--- infra/web.ts
+++ infra/web.ts
@@ -20,4 +20,9 @@ export const frontend = new sst.aws.StaticSite("Frontend", {
     VITE_IDENTITY_POOL_ID: identityPool.id,
     VITE_USER_POOL_CLIENT_ID: userPoolClient.id,
   },
+  domain: {
+    name: "www.comkite.com",
+    dns: false,
+    cert: "arn:aws:acm:us-east-1:376129882365:certificate/c2879544-e6fa-413e-b3b7-c12b180e30d1",
+  },
 });
